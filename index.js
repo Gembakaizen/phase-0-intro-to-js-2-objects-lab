04a873247e2cccfad41491e31f2603d8e6a5200a
Mohammed Yusuf <mohammedyusuf445@gmail.com>
2023-07-18 09:11:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee };
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+  }
+  const employee = {
+    name: 'Moha',
+    age: 22,
+    position: 'MARKETER'
+  };
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+ employee[key] = value;
+ return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const clonedEmployee = { ...employee };
+    delete clonedEmployee[key];
+    return clonedEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
